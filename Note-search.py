ce5e5fc946b9fa1e00fc38d449115031fdff1db3
Parts-Ai <163290401+Parts-Ai@users.noreply.github.com>
2024-03-18 23:12:26 +1100

    Update Note-search.py

--- Note-search.py
+++ Note-search.py
@@ -31,7 +31,7 @@ queryInputFromBox = st.text_input(label=" ", value="", help="Search for links in
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
+    st.secrets["service_account"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
     ],
